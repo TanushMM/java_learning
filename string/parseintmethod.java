2e4276107d433c76299f07963da11db8b8d4f349
Tanush M M <tanushmmofficial@gmail.com>
2024-01-30 18:15:15 +0530

    Added ParseIntMethod.java

---
+++ string/parseintmethod.java
@@ -0,0 +1,16 @@
+package string;
+
+public class parseintmethod {
+    public static void main(String args[]) {
+        // parseInt() belongs to Integer class
+        String str = "123";
+        int number = Integer.parseInt(str);
+        System.out.println(number);
+        
+        String str1 = "456";
+        number = Integer.parseInt(str1);
+        System.out.println(number);
+       
+    }
+ 
+}
