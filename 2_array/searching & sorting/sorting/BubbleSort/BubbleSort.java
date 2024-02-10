8f2b44e77c3bbdb642547ab32a1d86aba78407a4
Tanush M M <tanushmmofficial@gmail.com>
2024-02-10 12:51:19 +0530

    Updated BubbleSort.java

--- 2_array/searching & sorting/sorting/BubbleSort/BubbleSort.java
+++ 2_array/searching & sorting/sorting/BubbleSort/BubbleSort.java
@@ -1,5 +1,5 @@
 public class BubbleSort {
-    public static void bubbleSort(int[] array) {
+    public static void bubbleSortAscending(int[] array) {
         int length = array.length;
         for(int i =0; i < length-1; i++) {
             boolean swap = false;
@@ -15,17 +15,41 @@ public class BubbleSort {
             }
         }
     }
-    public static void main(String[] args) {
-        int[] array = {4,1,7,6,9,10};
+
+    public static void bubbleSortDescending(int[] array) {
+        for(int i =0; i < array.length-1; i++) {
+            boolean swap = false;
+            for(int j = 0;j < array.length-1-i; j++) {
+                if (array[j] < array[j+1]) {
+                    swap = true;
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+            if (swap == false) {
+                return;
+            } 
+        }
+    }
+
+
+    public static void printArray(int[] array) {
         for(int i =0; i < array.length; i++) {
             System.out.print(array[i] +" ");
         }
         System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] array = {4,1,7,6,9,10};
+        printArray(array);
 
-        bubbleSort(array);
+        bubbleSortAscending(array);
 
-        for(int i =0; i < array.length; i++) {
-            System.out.print(array[i] +" ");
-        }
+        printArray(array);
+
+        bubbleSortDescending(array);
+
+        printArray(array);
     }
 }
