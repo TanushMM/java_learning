1adf0f5ece34ccf3a99e01d6f2b60f6137615acc
Tanush M M <tanushmmofficial@gmail.com>
2024-02-10 12:54:46 +0530

    Updated SelectionSort.java

--- 2_array/searching & sorting/sorting/SelectionSort/SelectionSort.java
+++ 2_array/searching & sorting/sorting/SelectionSort/SelectionSort.java
@@ -1,8 +1,8 @@
 public class SelectionSort {
-    public static void selectionSort(int[] array) {
-        for(int i =0; i < array.length; i++) {
+    public static void selectionSortAscending(int[] array) {
+        for(int i =0; i < array.length-1; i++) {
             int minIndex = i;
-            for(int j = i; j < array.length; j++) {
+            for(int j = i+1; j < array.length; j++) {
                 if(array[j] < array[minIndex]) {
                     minIndex = j;
                 }
@@ -14,17 +14,39 @@ public class SelectionSort {
             }
         }
     }
-    public static void main(String[] args) {
-        int[] array = {4,1,7,6,9,10};
+
+    public static void selectionSortDescending(int[] array) {
+        for(int i = 0; i < array.length-1; i++) {
+            int maxIndex = i;
+            for(int j = i+1; j< array.length; j++) {
+                if (array[j] > array[maxIndex]) {
+                    maxIndex = j;
+                }
+            }
+            if(array[maxIndex] > array[i]) {
+                int temp = array[i];
+                array[i] = array[maxIndex];
+                array[maxIndex] = temp;
+            }
+        }
+    }
+
+    public static void printArray(int[] array) {
         for(int i =0; i < array.length; i++) {
             System.out.print(array[i] +" ");
         }
         System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] array = {4,1,7,6,9,10};
+        printArray(array);
 
-        selectionSort(array);
+        selectionSortAscending(array);
 
-        for(int i =0; i < array.length; i++) {
-            System.out.print(array[i] +" ");
-        }
+        printArray(array);
+
+        selectionSortDescending(array);
+
+        printArray(array);
     }
 }
