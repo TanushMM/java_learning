f81e4522582b0d2c330070ea9a7701007eae40e0
Tanush M M <tanushmmofficial@gmail.com>
2024-02-19 08:01:44 +0530

    Added PowerOf2OrNot.java

---
+++ 5_BitManipulation/problems/PowerOf2OrNot.java
@@ -0,0 +1,9 @@
+public class PowerOf2OrNot {
+    public static boolean powerOf2OrNot(int n) {
+        return (n & (n-1)) == 0;
+    }
+    public static void main(String[] args) {
+        System.out.println(powerOf2OrNot(32));
+        System.out.println(powerOf2OrNot(10));
+    }
+}
