b58a7408c94e29ce027757fd4f2d9d13da6ebbcc
Tanush M M <tanushmmofficial@gmail.com>
2024-03-18 17:33:05 +0530

    Added _1_ReverseLinkedList.java

---
+++ 11_LinkedList/Problems/_1_ReverseLinkedList.java
@@ -0,0 +1,42 @@
+public class _1_ReverseLinkedList {
+    public static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static void printList(Node head) {
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.data + " -> ");
+            currNode = currNode.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public static Node reverseIterative(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        Node prev = null;
+        Node curr = head;
+        Node nxt;
+        while (curr != null) {
+            nxt = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nxt;
+        }
+        return prev;
+    }
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+        printList(head);
+        printList(reverseIterative(head));
+    }    
+}
